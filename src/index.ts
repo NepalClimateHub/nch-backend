2527c92555a9dd4a045461a808a5502775c680a8
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2025-02-07 10:41:51 +0545

    feat: implement AuthModule for improved authentication handling

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,10 @@
 import { serve } from "@hono/node-server";
 import { logger } from "./shared/logger.js";
 import env from "./shared/env.js";
-import { authRouter } from "./app/module.js";
 import { app } from "./infrastructure/hono.js";
+import { authModule } from "./app/module.js";
 
-
-authRouter.configure();
+authModule.configure();
 
 const appPort = Number.parseInt(process.env.PORT as string) || 8080;
 
