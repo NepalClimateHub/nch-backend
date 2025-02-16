11ab8c058d305f74ca62852bac54fcc9b7f152bf
avashForReal <avash700@gmail.com>
2025-02-16 09:59:14 +0545

    feat: auth module

--- src/app/auth/auth.controller.ts
+++ src/app/auth/auth.controller.ts
@@ -2,8 +2,8 @@ import type { Context, Handler } from "hono";
 import type { RegisterIndividualRequest } from "./dto/register-input.dto.js";
 
 export const registerIndividualController: Handler = async (
-  c: Context
+	c: Context,
 ): Promise<Response> => {
-  const registerPayload = await c.req.json<RegisterIndividualRequest>();
-  return c.json({ message: "ok", response: registerPayload });
+	const registerPayload = await c.req.json<RegisterIndividualRequest>();
+	return c.json({ message: "ok", response: registerPayload });
 };
