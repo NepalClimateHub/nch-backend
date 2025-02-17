e011cd5d445907c9b0ca88c23b39d38379d44df6
Mukesh Kumar Chaudhary <mukezhz@gmail.com>
2025-02-17 16:45:00 +0545

    feat: enhance organization migration to include tags and improve TypeScript definitions

--- scripts/migrate_organzations.ts
+++ scripts/migrate_organzations.ts
@@ -1,5 +1,5 @@
 import cuid from "@bugsnag/cuid";
-import type { Organizations } from "../src/db/db.js";
+import type { Organizations, OrganizationTags } from "../src/db/db.js";
 import { db } from "../src/db/index.js";
 import OrganizationJSON from "./data/organizations.json" assert {
     type: "json",
@@ -8,13 +8,14 @@ import type { OrganizationDetail } from "./types.js";
 
 export async function migrateOrganizations() {
     const organizationJSON: OrganizationDetail[] = OrganizationJSON;
-    const invertedIndex: Record<string, Organizations> = {}
-    db.transaction().execute(async (tx) => {
+    const invertedOrganizationIndex: Record<string, Organizations> = {}
+
+    await db.transaction().execute(async (tx) => {
         const dbOrganizations = await tx.selectFrom("organizations").selectAll().execute()
         for (const dbOrganization of dbOrganizations) {
             if (dbOrganization.email) {
-                if (!invertedIndex[dbOrganization.email]) {
-                    invertedIndex[dbOrganization.email] = dbOrganization
+                if (!invertedOrganizationIndex[dbOrganization.email]) {
+                    invertedOrganizationIndex[dbOrganization.email] = dbOrganization
                 }
             }
         }
@@ -23,18 +24,20 @@ export async function migrateOrganizations() {
             const splittedAddress = address.split(",")
             const country = splittedAddress[splittedAddress.length - 1].trim()
             const city = splittedAddress.slice(0, splittedAddress.length - 1).join(",").trim()
-            invertedIndex[contact.email] = {
-                id: cuid(),
-                name: name,
-                description: description,
-                email: contact.email,
-                phoneNumber: contact.phone,
-                province: "",
-                city: city,
-                country: country,
-                publicImageSourceId: "",
-                publicImage: logoUrl,
-                slug: slug
+            if (!invertedOrganizationIndex[contact.email]) {
+                invertedOrganizationIndex[contact.email] = {
+                    id: cuid(),
+                    name: name,
+                    description: description,
+                    email: contact.email,
+                    phoneNumber: contact.phone,
+                    province: "",
+                    city: city,
+                    country: country,
+                    publicImageSourceId: "",
+                    publicImage: logoUrl,
+                    slug: slug
+                }
             }
         }
 
@@ -42,11 +45,37 @@ export async function migrateOrganizations() {
         await tx.deleteFrom("organizations").execute()
 
         console.log("inserting new organizations")
-        const organizationDaos = Object.values(invertedIndex)
+        const organizationDaos = Object.values(invertedOrganizationIndex)
         await tx
             .insertInto("organizations")
             .values(organizationDaos)
             .execute()
+    })
+
+    await db.transaction().execute(async (tx) => {
+        const organizationTags: OrganizationTags[] = []
+        const allDBTags = await tx.selectFrom("tags").selectAll().execute();
+        const invertTable: Record<string, (typeof allDBTags)[0]> = {};
+        for (const row of allDBTags) {
+            invertTable[row.tag] = row;
+        }
+        for (const organization of organizationJSON) {
+            const { tags, contact: { email } } = organization;
+            for (const tag of tags) {
+                if (invertTable[tag] && invertedOrganizationIndex[email]) {
+                    organizationTags.push({
+                        id: cuid(),
+                        organizationId: invertedOrganizationIndex[email].id,
+                        tagId: invertTable[tag].id
+                    })
+                }
+            }
+        }
+
+        console.log("removing old organization tags")
+        await tx.deleteFrom("organization_tags").execute()
+        console.log("inserting new organization tags")
+        tx.insertInto("organization_tags").values(organizationTags).execute()
     })
 
 }
